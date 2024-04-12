a08ece1684939d718f44464e98fb0cab53db495a
Kastor <kastorthecatboy@gmail.com>
2024-04-12 00:28:34 -0400

    - Added log parsing functionality so that we aren't relying on text searches. - Added handler for TOP P1 end floor flash - Adjusted README to reflect new repo

--- README.md
+++ README.md
@@ -4,29 +4,48 @@ This is a small ACT overlay that aims to reduce the seizure and headache inducin
 
 It works by looking for the casts of certain boss spells and applying a timed, semi-transparent black overlay to the screen to dampen the light.
 
-***PLEASE USE WITH CAUTION*** - While this plugin is made with the best intentions, I cannot gaurentee it will work for everyone and I cannot gaurentee it will always function.
+***PLEASE USE WITH CAUTION*** - While this plugin is made with the best intentions, I cannot guarantee it will work for everyone and I cannot gaurentee it will always function.
 Use your best judgement and don't get hurt for a video game.
 
-## List Of Effects By Encounter
+***BETA VERSION WARNING*** - The current version is in active development and may break unexpectedly.
+If you used or wish to use this plugin with the 2 encounters it supported previously, please view the old
+repository setup instructions [here](https://github.com/kastor-ffxiv/ACT-Photosense). Or use the [v0.1 release](https://github.com/SMNIRL/ACT-Photosense/releases/tag/v0.1) locally.
 
-This plugin currently works for the following mechanics, listed by encounter.
+## Table of Contents
+- [Setup/Installation](#installation)
+- [Contributing](#contributing)
+- [Supported Encounters](#supported-encounters)
+  - [Endwalker](#endwalker)
+  - [Shadowbringers](#shadowbringers)
 
-### The Tower At Paradigm's Breach (Nier Alliance Raid #3)
+# Supported Encounters
 
-#### Red Girl
+# Endwalker
+(Work in Progress)
+
+## The Omega Protocol (Ultimate)
+
+### Phase 1
+- Bright map change effect prior to Atomic Ray enrage - Darkens screen by 70% for 3 seconds.
+
+# Shadowbringers
+
+## The Tower At Paradigm's Breach (Nier Alliance Raid #3)
+
+### Red Girl
 - Cruelty - Darkens screen by 70% for 9 seconds.
 - Sublime Transcendence (AKA the Jumpscare) - Completely obscures screen for 10 seconds.
 
-#### False Idol/Her Inflorescence
+### False Idol/Her Inflorescence
 - Transition effect - Darkens screen by 70% for 12 seconds.
 - Distortion - Darkens screen by 70% for 9 seconds.
 
-### E12S
+## E12S
 
 - Lion Breath Cones - Darkens screen by 70% for 30 seconds after the Lion Sculptures spawn.
 
 
-## Setup/Installation
+# Installation
 
 1. If you do not yet have it installed, ACT is a requirement to use this overlay. You should install it by following the instructions here: https://github.com/FFXIV-ACT/setup-guide. You can skip the "FFLogs Uploader" section.
 
@@ -37,7 +56,7 @@ This plugin currently works for the following mechanics, listed by encounter.
 
 4. Click the `New` button (A), enter a name (B) (anything is fine), then select a Preset of `Custom` (C) and a Type of `MiniParse`. Then click `OK`. ![image](https://user-images.githubusercontent.com/73002107/114977719-24666b00-9e56-11eb-9c2d-6a4df9a199b0.png)
 
-5. Make sure the new Overlay you created is highlighted in the left panel (A), then enter `https://kastor-ffxiv.github.io/ACT-Photosense/` in the `URL` field (B) under the General tab, then check `Force white background` (C). *(You may have to scroll down, or enlarge the ACT window to see these options)* ![image](https://user-images.githubusercontent.com/73002107/114978289-09e0c180-9e57-11eb-8105-29830eddd1ce.png)
+5. Make sure the new Overlay you created is highlighted in the left panel (A), then enter `https://smnirl.github.io/ACT-Photosense/` in the `URL` field (B) under the General tab, then check `Force white background` (C). *(You may have to scroll down, or enlarge the ACT window to see these options)* ![image](https://user-images.githubusercontent.com/73002107/114978289-09e0c180-9e57-11eb-8105-29830eddd1ce.png)
 
 6. Now, a small white box should appear on your screen. ![image](https://user-images.githubusercontent.com/73002107/114978044-ab1b4800-9e56-11eb-830e-edb8e01d8740.png)
 
@@ -45,12 +64,15 @@ This plugin currently works for the following mechanics, listed by encounter.
 
 8. Uncheck `Force white background`, then check `Enable Clickthrough` (A) and `Lock Overlay` (B). ![image](https://user-images.githubusercontent.com/73002107/114979090-4365fc80-9e58-11eb-8485-bf5dfd6fca50.png)
 
-
 9. That's it! As long as you have ACT open while you play, the effects listed above should take place automatically.
 
 ## Contributing
 
 Pull Requests to this project are welcome! 
+
+If you wish to add an encounter or mechanic, please hold off until I modify the project structure. I plan to
+make different encounters their own ES6 classes so that configuration can be included to configure handlers
+as desired by the user.
 
 ## Planned Improvements
 - Customization, so users can easily turn on/off specific effects and adjust the darkness level of dampening.
--- zone_handlers.js
+++ zone_handlers.js
@@ -1,33 +1,209 @@
 let zoneHandlers = {
     909: e12S_handler,
     966: nier3_handler,
+    1122: omegaUltimate_handler
     //641: debug_handler
 };
 
-let spellTable = {
-    RedGirl_Cruelty: "6013",
-    RedGirl_SublimeTranscendence: "620A",
-    HerInflorescence_Distortion: "5BE9",
-    FalseIdol_Eminence: "5DD5"
+// https://github.com/OverlayPlugin/cactbot/blob/main/docs/LogGuide.md#overlayplugin-log-lines
+let logTable = {
+    ffxiv: {
+        game: "00",
+        changeZone: "01",
+        changePrimaryPlayer: "02",
+        addCombatant: "03",
+        removeCombatant: "04",
+        partyList: "11",
+        playerStats: "12",
+        networkStartsCasting: "20",
+        networkAbility: "21",
+        networkAOEAbility: "22",
+        networkCancelAbility: "23",
+        networkDoT: "24",
+        networkDeath: "25",
+        networkBuff: "26",
+        networkTargetIcon: "27",
+        networkRaidMarker: "28",
+        networkTargetMarker: "29",
+        networkBuffRemove: "30",
+        networkGauge: "31",
+        networkWorld: "32",
+        networkActorControl: "33",
+        networkNameToggle: "34",
+        networkTether: "35",
+        limitBreak: "36",
+        networkActionSync: "37",
+        networkStatusEffects: "38",
+        networkUpdateHP: "39",
+        map: "40",
+        systemLogMessage: "41",
+        statusList3: "42",
+        debug: "251",
+        packetDump: "252",
+        version: "253",
+        error: "254"
+    },
+    overlayPlugin: {
+        lineRegistration: "256",
+        mapEffect: "257",
+        fateDirector: "258",
+        CEDirector: "259",
+        inCombat: "260",
+        combatantMemory: "261",
+        RSVData: "262",
+        startsUsingExtra: "263",
+        abilityExtra: "264",
+        contentFinderSettings: "265",
+        npcYell: "266",
+        battleTalk2: "267",
+        countdown: "268",
+        countdownCancel: "269",
+        actorMove: "270",
+        actorSetPos: "271",
+        spawnNpcExtra: "272",
+        actorControlExtra: "273",
+        actorControlSelfExtra: "274",
+    }
+}
+
+const typeRegex = /^(\d+)|/;
+
+const logRegexTable = {
+    ffxiv: {
+        game: /^(?<type>00)\|(?<timestamp>[^|]*)\|(?<code>[^|]*)\|(?<name>[^|]*)\|(?<line>[^|]*)\|/,
+        changeZone: /^(?<type>01)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|/,
+        changePrimaryPlayer: /^(?<type>02)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|/,
+        addCombatant: /^(?<type>03)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|(?<job>[^|]*)\|(?<level>[^|]*)\|(?<ownerId>[^|]*)\|(?<worldId>[^|]*)\|(?<world>[^|]*)\|(?<npcNameId>[^|]*)\|(?<npcBaseId>[^|]*)\|(?<currentHp>[^|]*)\|(?<hp>[^|]*)\|(?<currentMp>[^|]*)\|(?<mp>[^|]*)\|(?:[^|]*\|){2}(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|(?<heading>[^|]*)\|/,
+        removeCombatant: /^(?<type>04)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|(?<job>[^|]*)\|(?<level>[^|]*)\|(?<owner>[^|]*)\|(?:[^|]*\|)(?<world>[^|]*)\|(?<npcNameId>[^|]*)\|(?<npcBaseId>[^|]*)\|(?:[^|]*\|)(?<hp>[^|]*)\|(?:[^|]*\|){4}(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|(?<heading>[^|]*)\|/,
+        partyList: /^(?<type>11)\|(?<timestamp>[^|]*)\|(?<partyCount>[^|]*)\|/,
+        playerStats: /^(?<type>12)\|(?<timestamp>[^|]*)\|(?<job>[^|]*)\|(?<strength>[^|]*)\|(?<dexterity>[^|]*)\|(?<vitality>[^|]*)\|(?<intelligence>[^|]*)\|(?<mind>[^|]*)\|(?<piety>[^|]*)\|(?<attackPower>[^|]*)\|(?<directHit>[^|]*)\|(?<criticalHit>[^|]*)\|(?<attackMagicPotency>[^|]*)\|(?<healMagicPotency>[^|]*)\|(?<determination>[^|]*)\|(?<skillSpeed>[^|]*)\|(?<spellSpeed>[^|]*)\|(?:[^|]*\|)(?<tenacity>[^|]*)\|(?<localContentId>[^|]*)\|/,
+        networkStartsCasting: /^(?<type>20)\|(?<timestamp>[^|]*)\|(?<sourceId>[^|]*)\|(?<source>[^|]*)\|(?<id>[^|]*)\|(?<ability>[^|]*)\|(?<targetId>[^|]*)\|(?<target>[^|]*)\|(?<castTime>[^|]*)\|(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|(?<heading>[^|]*)\|/,
+        networkAbility: /^(?<type>2[12])\|(?<timestamp>[^|]*)\|(?<sourceId>[^|]*)\|(?<source>[^|]*)\|(?<id>[^|]*)\|(?<ability>[^|]*)\|(?<targetId>[^|]*)\|(?<target>[^|]*)\|(?<flags>[^|]*)\|(?<damage>[^|]*)\|(?:[^|]*\|){14}(?<targetCurrentHp>[^|]*)\|(?<targetMaxHp>[^|]*)\|(?<targetCurrentMp>[^|]*)\|(?<targetMaxMp>[^|]*)\|(?:[^|]*\|){2}(?<targetX>[^|]*)\|(?<targetY>[^|]*)\|(?<targetZ>[^|]*)\|(?<targetHeading>[^|]*)\|(?<currentHp>[^|]*)\|(?<maxHp>[^|]*)\|(?<currentMp>[^|]*)\|(?<maxMp>[^|]*)\|(?:[^|]*\|){2}(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|(?<heading>[^|]*)\|(?<sequence>[^|]*)\|(?<targetIndex>[^|]*)\|(?<targetCount>[^|]*)\|/,
+        networkAOEAbility: /^(?<type>2[12])\|(?<timestamp>[^|]*)\|(?<sourceId>[^|]*)\|(?<source>[^|]*)\|(?<id>[^|]*)\|(?<ability>[^|]*)\|(?<targetId>[^|]*)\|(?<target>[^|]*)\|(?<flags>[^|]*)\|(?<damage>[^|]*)\|(?:[^|]*\|){14}(?<targetCurrentHp>[^|]*)\|(?<targetMaxHp>[^|]*)\|(?<targetCurrentMp>[^|]*)\|(?<targetMaxMp>[^|]*)\|(?:[^|]*\|){2}(?<targetX>[^|]*)\|(?<targetY>[^|]*)\|(?<targetZ>[^|]*)\|(?<targetHeading>[^|]*)\|(?<currentHp>[^|]*)\|(?<maxHp>[^|]*)\|(?<currentMp>[^|]*)\|(?<maxMp>[^|]*)\|(?:[^|]*\|){2}(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|(?<heading>[^|]*)\|(?<sequence>[^|]*)\|(?<targetIndex>[^|]*)\|(?<targetCount>[^|]*)\|/,
+        networkCancelAbility: /^(?<type>23)\|(?<timestamp>[^|]*)\|(?<sourceId>[^|]*)\|(?<source>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|(?<reason>[^|]*)\|/,
+        networkDoT: /^(?<type>24)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|(?<which>[^|]*)\|(?<effectId>[^|]*)\|(?<damage>[^|]*)\|(?<currentHp>[^|]*)\|(?<maxHp>[^|]*)\|(?<currentMp>[^|]*)\|(?<maxMp>[^|]*)\|(?:[^|]*\|){2}(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|(?<heading>[^|]*)\|(?<sourceId>[^|]*)\|(?<source>[^|]*)\|(?<damageType>[^|]*)\|(?<sourceCurrentHp>[^|]*)\|(?<sourceMaxHp>[^|]*)\|(?<sourceCurrentMp>[^|]*)\|(?<sourceMaxMp>[^|]*)\|(?:[^|]*\|){2}(?<sourceX>[^|]*)\|(?<sourceY>[^|]*)\|(?<sourceZ>[^|]*)\|(?<sourceHeading>[^|]*)\|/,
+        networkDeath: /^(?<type>25)\|(?<timestamp>[^|]*)\|(?<targetId>[^|]*)\|(?<target>[^|]*)\|(?<sourceId>[^|]*)\|(?<source>[^|]*)\|/,
+        networkBuff: /^(?<type>26)\|(?<timestamp>[^|]*)\|(?<effectId>[^|]*)\|(?<effect>[^|]*)\|(?<duration>[^|]*)\|(?<sourceId>[^|]*)\|(?<source>[^|]*)\|(?<targetId>[^|]*)\|(?<target>[^|]*)\|(?<count>[^|]*)\|(?<targetMaxHp>[^|]*)\|(?<sourceMaxHp>[^|]*)\|/,
+        networkTargetIcon: /^(?<type>27)\|(?<timestamp>[^|]*)\|(?<targetId>[^|]*)\|(?<target>[^|]*)\|(?:[^|]*\|){2}(?<id>[^|]*)\|/,
+        networkRaidMarker: /^(?<type>28)\|(?<timestamp>[^|]*)\|(?<operation>[^|]*)\|(?<waymark>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|/,
+        networkTargetMarker: /^(?<type>29)\|(?<timestamp>[^|]*)\|(?<operation>[^|]*)\|(?<waymark>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|(?<targetId>[^|]*)\|(?<targetName>[^|]*)\|/,
+        networkBuffRemove: /^(?<type>30)\|(?<timestamp>[^|]*)\|(?<effectId>[^|]*)\|(?<effect>[^|]*)\|(?:[^|]*\|)(?<sourceId>[^|]*)\|(?<source>[^|]*)\|(?<targetId>[^|]*)\|(?<target>[^|]*)\|(?<count>[^|]*)\|/,
+        networkGauge: /^(?<type>31)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<data0>[^|]*)\|(?<data1>[^|]*)\|(?<data2>[^|]*)\|(?<data3>[^|]*)\|/,
+        networkActorControl: /^(?<type>33)\|(?<timestamp>[^|]*)\|(?<instance>[^|]*)\|(?<command>[^|]*)\|(?<data0>[^|]*)\|(?<data1>[^|]*)\|(?<data2>[^|]*)\|(?<data3>[^|]*)\|/,
+        networkNameToggle: /^(?<type>34)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|(?<targetId>[^|]*)\|(?<targetName>[^|]*)\|(?<toggle>[^|]*)\|/,
+        networkTether: /^(?<type>35)\|(?<timestamp>[^|]*)\|(?<sourceId>[^|]*)\|(?<source>[^|]*)\|(?<targetId>[^|]*)\|(?<target>[^|]*)\|(?:[^|]*\|){2}(?<id>[^|]*)\|/,
+        limitBreak: /^(?<type>36)\|(?<timestamp>[^|]*)\|(?<valueHex>[^|]*)\|(?<bars>[^|]*)\|/,
+        networkActionSync: /^(?<type>37)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|(?<sequenceId>[^|]*)\|(?<currentHp>[^|]*)\|(?<maxHp>[^|]*)\|(?<currentMp>[^|]*)\|(?<maxMp>[^|]*)\|(?<currentShield>[^|]*)\|(?:[^|]*\|)(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|(?<heading>[^|]*)\|/,
+        networkStatusEffects: /^(?<type>38)\|(?<timestamp>[^|]*)\|(?<targetId>[^|]*)\|(?<target>[^|]*)\|(?<jobLevelData>[^|]*)\|(?<hp>[^|]*)\|(?<maxHp>[^|]*)\|(?<mp>[^|]*)\|(?<maxMp>[^|]*)\|(?<currentShield>[^|]*)\|(?:[^|]*\|)(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|(?<heading>[^|]*)\|(?<data0>[^|]*)\|(?<data1>[^|]*)\|(?<data2>[^|]*)\|/,
+        networkUpdateHP: /^(?<type>39)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|(?<currentHp>[^|]*)\|(?<maxHp>[^|]*)\|(?<currentMp>[^|]*)\|(?<maxMp>[^|]*)\|(?:[^|]*\|){2}(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|(?<heading>[^|]*)\|/,
+        map: /^(?<type>40)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<regionName>[^|]*)\|(?<placeName>[^|]*)\|(?<placeNameSub>[^|]*)\|/,
+        systemLogMessage: /^(?<type>41)\|(?<timestamp>[^|]*)\|(?<instance>[^|]*)\|(?<id>[^|]*)\|(?<param0>[^|]*)\|(?<param1>[^|]*)\|(?<param2>[^|]*)\|/,
+        statusList3: /^(?<type>42)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<name>[^|]*)\|/,
+    },
+    overlayPlugin: {
+        lineRegistration: /^(?<type>256)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<source>[^|]*)\|(?<version>[^|]*)\|/,
+        mapEffect: /^(?<type>257)\|(?<timestamp>[^|]*)\|(?<instance>[^|]*)\|(?<flags>[^|]*)\|(?<location>[^|]*)\|(?<data0>[^|]*)\|(?<data1>[^|]*)\|/,
+        fateDirector: /^(?<type>258)\|(?<timestamp>[^|]*)\|(?<category>[^|]*)\|(?:[^|]*\|)(?<fateId>[^|]*)\|(?<progress>[^|]*)\|/,
+        CEDirector: /^(?<type>259)\|(?<timestamp>[^|]*)\|(?<popTime>[^|]*)\|(?<timeRemaining>[^|]*)\|(?:[^|]*\|)(?<ceKey>[^|]*)\|(?<numPlayers>[^|]*)\|(?<status>[^|]*)\|(?:[^|]*\|)(?<progress>[^|]*)\|/,
+        inCombat: /^(?<type>260)\|(?<timestamp>[^|]*)\|(?<inACTCombat>[^|]*)\|(?<inGameCombat>[^|]*)\|(?<isACTChanged>[^|]*)\|(?<isGameChanged>[^|]*)\|/,
+        combatantMemory: /^(?<type>261)\|(?<timestamp>[^|]*)\|(?<change>[^|]*)\|(?<id>[^|]*)\|(?:AggressionStatus\|(?<pairAggressionStatus>[^|]*)\|)?(?:BNpcID\|(?<pairBNpcID>[^|]*)\|)?(?:BNpcNameID\|(?<pairBNpcNameID>[^|]*)\|)?(?:CastBuffID\|(?<pairCastBuffID>[^|]*)\|)?(?:CastDurationCurrent\|(?<pairCastDurationCurrent>[^|]*)\|)?(?:CastDurationMax\|(?<pairCastDurationMax>[^|]*)\|)?(?:CastGroundTargetX\|(?<pairCastGroundTargetX>[^|]*)\|)?(?:CastGroundTargetY\|(?<pairCastGroundTargetY>[^|]*)\|)?(?:CastGroundTargetZ\|(?<pairCastGroundTargetZ>[^|]*)\|)?(?:CastTargetID\|(?<pairCastTargetID>[^|]*)\|)?(?:CurrentCP\|(?<pairCurrentCP>[^|]*)\|)?(?:CurrentGP\|(?<pairCurrentGP>[^|]*)\|)?(?:CurrentHP\|(?<pairCurrentHP>[^|]*)\|)?(?:CurrentMP\|(?<pairCurrentMP>[^|]*)\|)?(?:CurrentWorldID\|(?<pairCurrentWorldID>[^|]*)\|)?(?:Distance\|(?<pairDistance>[^|]*)\|)?(?:EffectiveDistance\|(?<pairEffectiveDistance>[^|]*)\|)?(?:Heading\|(?<pairHeading>[^|]*)\|)?(?:ID\|(?<pairID>[^|]*)\|)?(?:IsCasting1\|(?<pairIsCasting1>[^|]*)\|)?(?:IsCasting2\|(?<pairIsCasting2>[^|]*)\|)?(?:IsTargetable\|(?<pairIsTargetable>[^|]*)\|)?(?:Job\|(?<pairJob>[^|]*)\|)?(?:Level\|(?<pairLevel>[^|]*)\|)?(?:MaxCP\|(?<pairMaxCP>[^|]*)\|)?(?:MaxGP\|(?<pairMaxGP>[^|]*)\|)?(?:MaxHP\|(?<pairMaxHP>[^|]*)\|)?(?:MaxMP\|(?<pairMaxMP>[^|]*)\|)?(?:ModelStatus\|(?<pairModelStatus>[^|]*)\|)?(?:MonsterType\|(?<pairMonsterType>[^|]*)\|)?(?:Name\|(?<pairName>[^|]*)\|)?(?:NPCTargetID\|(?<pairNPCTargetID>[^|]*)\|)?(?:OwnerID\|(?<pairOwnerID>[^|]*)\|)?(?:PartyType\|(?<pairPartyType>[^|]*)\|)?(?:PCTargetID\|(?<pairPCTargetID>[^|]*)\|)?(?:PosX\|(?<pairPosX>[^|]*)\|)?(?:PosY\|(?<pairPosY>[^|]*)\|)?(?:PosZ\|(?<pairPosZ>[^|]*)\|)?(?:Radius\|(?<pairRadius>[^|]*)\|)?(?:Status\|(?<pairStatus>[^|]*)\|)?(?:TargetID\|(?<pairTargetID>[^|]*)\|)?(?:TransformationId\|(?<pairTransformationId>[^|]*)\|)?(?:Type\|(?<pairType>[^|]*)\|)?(?:WeaponId\|(?<pairWeaponId>[^|]*)\|)?(?:WorldID\|(?<pairWorldID>[^|]*)\|)?(?:WorldName\|(?<pairWorldName>[^|]*)\|)?/,
+        RSVData: /^(?<type>262)\|(?<timestamp>[^|]*)\|(?<locale>[^|]*)\|(?:[^|]*\|)(?<key>[^|]*)\|(?<value>[^|]*)\|/,
+        startsUsingExtra: /^(?<type>263)\|(?<timestamp>[^|]*)\|(?<sourceId>[^|]*)\|(?<id>[^|]*)\|(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|/,
+        abilityExtra: /^(?<type>264)\|(?<timestamp>[^|]*)\|(?<sourceId>[^|]*)\|(?<id>[^|]*)\|(?<globalEffectCounter>[^|]*)\|(?<dataFlag>[^|]*)\|(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|/,
+        contentFinderSettings: /^(?<type>265)\|(?<timestamp>[^|]*)\|(?<zoneId>[^|]*)\|(?<zoneName>[^|]*)\|(?<inContentFinderContent>[^|]*)\|(?<unrestrictedParty>[^|]*)\|(?<minimalItemLevel>[^|]*)\|(?<silenceEcho>[^|]*)\|(?<explorerMode>[^|]*)\|(?<levelSync>[^|]*)\|/,
+        npcYell: /^(?<type>266)\|(?<timestamp>[^|]*)\|(?<npcId>[^|]*)\|(?<npcNameId>[^|]*)\|(?<npcYellId>[^|]*)\|/,
+        battleTalk2: /^(?<type>267)\|(?<timestamp>[^|]*)\|(?<npcId>[^|]*)\|(?<instance>[^|]*)\|(?<npcNameId>[^|]*)\|(?<instanceContentTextId>[^|]*)\|(?<displayMs>[^|]*)\|/,
+        countdown: /^(?<type>268)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<worldId>[^|]*)\|(?<countdownTime>[^|]*)\|(?<result>[^|]*)\|(?<name>[^|]*)\|/,
+        countdownCancel: /^(?<type>269)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<worldId>[^|]*)\|(?<name>[^|]*)\|/,
+        actorMove: /^(?<type>270)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<heading>[^|]*)\|(?:[^|]*\|){2}(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|/,
+        actorSetPos: /^(?<type>271)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<heading>[^|]*)\|(?:[^|]*\|){2}(?<x>[^|]*)\|(?<y>[^|]*)\|(?<z>[^|]*)\|/,
+        spawnNpcExtra: /^(?<type>272)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<parentId>[^|]*)\|(?<tetherId>[^|]*)\|(?<animationState>[^|]*)\|/,
+        actorControlExtra: /^(?<type>273)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<category>[^|]*)\|(?<param1>[^|]*)\|(?<param2>[^|]*)\|(?<param3>[^|]*)\|(?<param4>[^|]*)\|/,
+        actorControlSelfExtra: /^(?<type>274)\|(?<timestamp>[^|]*)\|(?<id>[^|]*)\|(?<category>[^|]*)\|(?<param1>[^|]*)\|(?<param2>[^|]*)\|(?<param3>[^|]*)\|(?<param4>[^|]*)\|(?<param5>[^|]*)\|(?<param6>[^|]*)\|/,
+    }
+
+}
+
+/**
+ * parse raw log data
+ * @param logLine
+ * @returns ?Object
+ */
+function parseLogLine(logLine) {
+    const typeMatch = logLine.match(typeRegex);
+    const logType = typeMatch[0];
+    let type;
+    let parseRegex;
+
+    // Search for type
+    if(parseInt(logType) > 255) {
+        type = Object.keys(logTable.overlayPlugin)[Object.values(logTable.overlayPlugin).indexOf(logType)];
+        if(!logRegexTable.overlayPlugin.hasOwnProperty(type)) {
+            console.error('Error: Could not determine log type. Did you copy the line correctly?')
+            return null;
+        }
+        parseRegex = logRegexTable.overlayPlugin[type];
+    } else {
+        type = Object.keys(logTable.ffxiv)[Object.values(logTable.ffxiv).indexOf(logType)];
+        if(!logRegexTable.ffxiv.hasOwnProperty(type)) {
+            console.error('Error: Could not determine log type. Did you copy the line correctly?')
+            return null;
+        }
+        parseRegex = logRegexTable.ffxiv[type];
+    }
+
+    const logMatch = logLine.match(parseRegex);
+
+    if(!logMatch.hasOwnProperty('groups')) {
+        console.error('Error: Failed to parse log line.')
+    }
+
+    let logData = logMatch.groups;
+
+    logData.type = type;
+    logData.typeId = logType;
+
+    return logData;
+}
+
+/**
+ * @todo: debug this lol
+ * @param lineObj Array
+ * @param onlyTypes String
+ * @returns ?object
+ */
+function getLine(lineObj, onlyTypes = null) {
+    if(!lineObj.hasOwnProperty('line') && !Array.isArray(lineObj.line)) {
+        return null;
+    }
+    let lineArray = lineObj.line;
+
+    let log = {
+        type: lineArray[0]
+    }
+
+    // If we only want one type of log, bail out when we handle another type
+    if(onlyTypes !== null) {
+        if(!onlyTypes.find((f) => f === log.type)) {
+            return null;
+        }
+    }
+
+    return parseLogLine(lineObj.rawLine);
 }
 
 function debug_handler(e) {
     // Loads in Shirogane only
-        if(Array.isArray(e.line) && e.line[0] == "00") {
-        e.line.forEach((line) => {
-            if(line.includes('Ruin II')) {
-                console.log("!Debug Listener Called!")
+    let line = getLine(e);
 
-                effect_fadeOutIn(10, 0.7);
-            }
-
-            if(line.includes('gain the effect')) {
-                console.log(e.line);
-            }
-        });
-    }
+    // Show me everything
+    console.log(line);
 }
 
+
+/**
+ * @todo Refactor to make use of logTable structs so we aren't doing string parsing of the entire logLine object
+ */
 function e12S_handler(e) {
     if(Array.isArray(e.line)) {
         e.line.forEach((line) => {
@@ -39,21 +215,45 @@ function e12S_handler(e) {
 }
 
 function nier3_handler(e) {
-	if(Array.isArray(e.line)) {
-    	if(e.line[0] == "20" && e.line[4] == spellTable.RedGirl_Cruelty) {
-    		effect_fadeOutIn(9, 0.7);
-    	}
-
-        if(e.line[0] == "20" && e.line[4] == spellTable.RedGirl_SublimeTranscendence) {
-            effect_fadeOutIn(10, 1);
-        }
-
-        if(e.line[0] == "20" && e.line[4] == spellTable.HerInflorescence_Distortion) {
-            effect_fadeOutIn(9, 0.7);
-        }
-
-        if(e.line[0] == "20" && e.line[4] == spellTable.FalseIdol_Eminence) {
-            effect_fadeOutIn(12, 0.7);
-        }
+    let spellTable = {
+        RedGirl_Cruelty: "6013",
+        RedGirl_SublimeTranscendence: "620A",
+        HerInflorescence_Distortion: "5BE9",
+        FalseIdol_Eminence: "5DD5"
     }
+
+    let line = getLine(e, [logTable.ffxiv.networkStartsCasting]);
+    if(!line) {
+        return;
+    }
+
+    switch(line.id) {
+        case spellTable.RedGirl_Cruelty:
+            effect_fadeOutIn(9, 0.7);
+            break;
+        case spellTable.RedGirl_SublimeTranscendence:
+            effect_fadeOutIn(10, 1);
+            break;
+        case spellTable.HerInflorescence_Distortion:
+            effect_fadeOutIn(9, 0.7);
+            break;
+        case spellTable.FalseIdol_Eminence:
+            effect_fadeOutIn(12, 0.7);
+            break;
+    }
+}
+
+function omegaUltimate_handler(e) {
+    let line = getLine(e, [logTable.overlayPlugin.mapEffect]);
+
+    if(!line) {
+        return;
+    }
+
+    // Bright arena effect prior to Atomic Ray enrage
+    if(line.typeId == logTable.overlayPlugin.mapEffect && line.flags == '00080004' && line.location == '09') {
+        console.log('firing real');
+        effect_fadeOutIn(2, 0.7);
+    }
+
 }
